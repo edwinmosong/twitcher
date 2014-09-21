e7daf7d7ce59076714018fb26309ee7e2139f710
Edwin Mo <edwin.mo@live.com>
2014-09-21 00:41:16 -0700

    Had tabs instead of spaces, and a few errors in extraction.

--- twitcher/__init__.py
+++ twitcher/__init__.py
@@ -16,39 +16,39 @@ __version__ = '0.0.1'
 
 BASE_URL = 'https://api.twitch.tv/kraken'
 GET_API_PATHS = {
-	'streams':				'/streams/',
-	'streams_channel': 		'/streams/%s/',
-	'streams_feature':		'/streams/featured',
-	'streams_summary':		'/streams/summary',
-	'channel':				'/channels/%s',
-	'channel_videos':		'/channels/%s/videos',
-	'channel_follows':		'/channels/%s/follows'
+    'streams':              '/streams/',
+    'streams_channel':      '/streams/%s/',
+    'streams_feature':      '/streams/featured',
+    'streams_summary':      '/streams/summary',
+    'channel':              '/channels/%s',
+    'channel_videos':       '/channels/%s/videos',
+    'channel_follows':      '/channels/%s/follows'
 }
 
 class Twitcher(object):
-	""" 
-	TwitchTV client that is used to make and send requests. This class wraps
-	requests and returns objects.
-	"""
-	def __init__(self):
-		self.rest_helper = helper.RESTHelper()
+    """ 
+    TwitchTV client that is used to make and send requests. This class wraps
+    requests and returns objects.
+    """
+    def __init__(self):
+        self.rest_helper = helper.RESTHelper()
 
-	def get_streams(self):
-		"""
-		Returns a list of Stream objects that are currently online on TwitchTV.
+    def get_streams(self):
+        """
+        Returns a list of Stream objects that are currently online on TwitchTV.
 
-		:returns: a list of Stream objects
-		"""
-		pass
+        :returns: a list of Stream objects
+        """
+        pass
 
-	def get_stream_info(self, channel):
-		"""
-		Returns a Stream object for channel
+    def get_stream_info(self, channel):
+        """
+        Returns a Stream object for channel
 
-		:params: channel - string of name for the stream's channel
-		:returns: classes.Stream object
-		"""
-		endpoint = GET_API_PATHS['streams_channel'] % channel
-		stream_info = self.rest_helper.request(endpoint=endpoint)
-		return classes.Stream(stream_info)
+        :params: channel - string of name for the stream's channel
+        :returns: classes.Stream object
+        """
+        endpoint = GET_API_PATHS['streams_channel'] % channel
+        stream_info = self.rest_helper.request(endpoint=endpoint)
+        return classes.Stream(stream_info)
 
--- twitcher/helper.py
+++ twitcher/helper.py
@@ -9,35 +9,35 @@ BASE_URL = 'https://api.twitch.tv/kraken'
 
 
 class RESTException(Exception):
-	def __init__(self, msg):
-		super(RESTException, self).__init__(self, msg)
+    def __init__(self, msg):
+        super(RESTException, self).__init__(self, msg)
 
 
 class RESTHelper(object):
-	"""
-	Helper class to wrap REST calls
-	"""
-	def __init__(self):
-		pass
+    """
+    Helper class to wrap REST calls
+    """
+    def __init__(self):
+        pass
 
-	def request(self, verb='GET', url=BASE_URL, endpoint='', headers=None):
-		"""
-		Wraps requests. Sets defaults for verb(GET) and 
-		url('https://api.twitch.tv/kraken')
-		"""
-		target = url + endpoint
-		if verb.upper() == 'GET':
-			response = requests.get(target, headers=headers)
-			if response.status_code != 200:
-				raise RESTException('Failed attempting to %s: %s' % (verb, 
-					target))
-		elif verb.upper() == 'PUT':
-			response = requests.put(target, headers=headers)
-			if response.status_code != 200:
-				raise RESTException('Failed attempting to %s: %s' % (verb, 
-					target))
-		else: # only supporting GET and PUT for now
-			raise RESTException('Attempted to request', verb.upper(), 'but only\
-				GET and PUT are supported.')
+    def request(self, verb='GET', url=BASE_URL, endpoint='', headers=None):
+        """
+        Wraps requests. Sets defaults for verb(GET) and 
+        url('https://api.twitch.tv/kraken')
+        """
+        target = url + endpoint
+        if verb.upper() == 'GET':
+            response = requests.get(target, headers=headers)
+            if response.status_code != 200:
+                raise RESTException('Failed attempting to %s: %s' % (verb, 
+                    target))
+        elif verb.upper() == 'PUT':
+            response = requests.put(target, headers=headers)
+            if response.status_code != 200:
+                raise RESTException('Failed attempting to %s: %s' % (verb, 
+                    target))
+        else: # only supporting GET and PUT for now
+            raise RESTException('Attempted to request', verb.upper(), 'but only\
+                GET and PUT are supported.')
 
-		return json.loads(response.content)
+        return json.loads(response.content)
